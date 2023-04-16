7a78e62d7ddc11968f84f6bab4fa24e71a92fd96
Satwick17 <81343072+Satwick17@users.noreply.github.com>
2023-04-17 02:03:07 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate(){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if(username == "Phuspa" && password == "1234")
+    if(username == "Phuspa@gmail.com" && password == "1234")
     {
        
         window.open("game.html");
